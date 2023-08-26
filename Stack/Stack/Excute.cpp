7ef268abe914dbcc2ca936d3275d063f1b6f51e7
Nam's_Desktop <vbnb0123@naver.com>
2023-08-26 15:14:17 +0900

    Stack 실행 코드

--- Stack/Stack/Excute.cpp
+++ Stack/Stack/Excute.cpp
@@ -1,6 +1,33 @@
 ﻿#include "Stack.h"
 
+typedef ListStack<int> Stack;
+
 int main()
 {
-    std::cout << "Hello World!\n";
+	Stack* stack = new Stack();
+	for (int i = 1; i <= 7; i++)
+	{
+		int* index = new int;
+		*index = *(&i);
+		stack->Push_Back(Stack::Create(&index));
+	}
+
+	stack->Print();
+
+	cout << stack->GetSize() << endl << endl;
+
+	ListStack<int>::Node* node = stack->Pop_Back();
+
+	cout << *node->Data << endl;
+	
+	stack->Print();
+
+	cout << stack->GetSize() << endl << endl;
+
+	stack->RemoveAll();
+
+	cout << stack->GetSize() << endl << endl;
+
+
+	return 0;
 }
